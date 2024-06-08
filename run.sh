318cd80c46ff9b1a7e6063150dd7b12f30bf3086
Jokeren <robinho364@gmail.com>
2024-06-08 17:46:11 -0400

    [DEV] More fixes to script

--- run.sh
+++ run.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-PROFILERS=("nsys profile" "proton")
+PROFILERS=("nsys" "proton")
 MODES=("cpu_bound" "gpu_bound")
-SCRIPTS=("bench_torch.py" "python bench_triton.py")
+SCRIPTS=("bench_torch.py" "bench_triton.py")
 
 for profiler in "${PROFILERS[@]}"
 do
-		for mode in "${MODES[@]}"
-		do
-				for script in "${SCRIPTS[@]}"
-				do
-						echo "Running $profiler $script $mode"
-						$profiler "$script" "$mode" 
-				done
-		done
+  for mode in "${MODES[@]}"
+  do
+    for script in "${SCRIPTS[@]}"
+    do
+      echo "Running $profiler $script $mode"
+      if [ "$profiler" == "nsys" ]
+      then
+        time nsys profile python "$script" "$mode"
+      elif [ "$profiler" == "proton" ]
+      then
+        time proton "$script" "$mode"
+      fi
+    done
+  done
 done
