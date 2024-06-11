d0497a56209ba0d345af0f08d83e8e68318ee667
Jokeren <robinho364@gmail.com>
2024-06-10 22:29:08 -0400

    [DEV] Fix bugs

--- microbench.py
+++ microbench.py
@@ -4,7 +4,7 @@ import argparse
 import triton
 import triton.language as tl
 
-BLOCK_SIZE = 1024
+BLOCK_SIZE = 256
 
 
 @triton.jit
@@ -13,9 +13,11 @@ def triton_dot(a_ptr, b_ptr, c_ptr, BLOCK_SIZE: tl.constexpr):
     offs = tl.arange(0, BLOCK_SIZE)
     a = tl.load(a_ptr + pid * BLOCK_SIZE + offs)
     b = tl.load(b_ptr + pid * BLOCK_SIZE + offs)
-    a = tl.broadcast(a[:, None], (BLOCK_SIZE, BLOCK_SIZE))
-    b = tl.broadcast(b[None, :], (BLOCK_SIZE, BLOCK_SIZE))
-    c = tl.sum(tl.dot(a, b), axis=1)
+    c = tl.zeros_like(a)
+    a = tl.broadcast_to(a[:, None], (BLOCK_SIZE, 16))
+    b = tl.broadcast_to(b[None, :], (16, BLOCK_SIZE))
+    for _ in range(10000):
+        c = tl.sum(tl.dot(a, b), axis=1) + c
     tl.store(c_ptr + pid * BLOCK_SIZE + offs, c)
 
 
@@ -41,16 +43,15 @@ def run(nelems, iters, kernel, workload):
             result_gpu = torch.empty_like(tensor_a)
             if workload == "cpu_bound":
                 triton_add[(nelems // BLOCK_SIZE, )](tensor_a, tensor_b,
-                                                     result_gpu, BLOCK_SIZE)
+                        result_gpu, BLOCK_SIZE)
             else:
                 triton_dot[(nelems // BLOCK_SIZE, )](tensor_a, tensor_b,
-                                                     result_gpu, BLOCK_SIZE)
+                        result_gpu, BLOCK_SIZE)
         elif kernel == "torch":
             if workload == "cpu_bound":
                 result_gpu = tensor_a + tensor_b
             else:
-                result_gpu = torch.matmul(
-                    tensor_a[:, None], tensor_b[None, :]).sum(dim=1)
+                result_gpu = torch.matmul(tensor_a[:,None],tensor_b[None,:]).sum(dim=1)
 
     # warmup
     add()
@@ -59,25 +60,25 @@ def run(nelems, iters, kernel, workload):
     # measure
     for _ in range(iters):
         add()
-    end_time = time.time()
 
     torch.cuda.synchronize()
 
+    end_time = time.time()
     print("cpu time:", end_time - start_time)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--kernel", type=str,
-                        choices=["torch", "triton"], required=True)
+            choices=["torch", "triton"], required=True)
     parser.add_argument("--workload", type=str,
-                        choices=["cpu_bound", "gpu_bound"], required=True)
+            choices=["cpu_bound", "gpu_bound"], required=True)
     parser.add_argument("--profiler", type=str,
-                        choices=["nsys", "proton", "kineto", "none"], default="none")
+            choices=["nsys", "proton", "kineto", "none"], default="none")
     args = parser.parse_args()
     if args.workload == "cpu_bound":
         run(nelems=BLOCK_SIZE, iters=100000,
-            kernel=args.kernel, workload=args.workload)
+                kernel=args.kernel, workload=args.workload)
     elif args.workload == "gpu_bound":
-        run(nelems=BLOCK_SIZE*10, iters=100,
-            kernel=args.kernel, workload=args.workload)
+        run(nelems=BLOCK_SIZE*100, iters=1000,
+                kernel=args.kernel, workload=args.workload)
