6f7a6ba76026a84dedd9c917de69456c814d5472
Jokeren <robinho364@gmail.com>
2025-12-24 02:44:32 +0000

    Add cudagraph bench

---
+++ cudagraph.py
@@ -0,0 +1,54 @@
+import torch 
+import triton 
+import triton.profiler as proton 
+import time
+
+
+def fn(num_scopes=5000): 
+    device = "cuda" 
+    for i in range(num_scopes):
+        with proton.scope(f"scope_{i}"):
+            x = torch.randn(512, 512, device=device) 
+            y = torch.randn(512, 512, device=device) 
+            z = torch.relu(x @ y) 
+
+
+def run(profiling=False, num_iters=100):
+    if profiling:
+        session = proton.start(f"profile") 
+
+    # warmup
+    fn()
+
+    stream = torch.cuda.Stream()
+    torch.cuda.set_stream(stream)
+    g = torch.cuda.CUDAGraph()
+    with torch.cuda.graph(g):
+        fn()
+
+    start_time = time.time()
+
+    for i in range(num_iters): 
+        g.replay()
+
+        if profiling and i % num_iters == num_iters - 1:
+            time0 = time.time()
+            proton.deactivate(session)
+            print("deactivate time: ", time.time() - time0)
+            time0 = time.time()
+            proton.get_data_msgpack(session)
+            print("get_data time: ", time.time() - time0)
+            proton.clear_data(session)
+            proton.activate(session)
+
+    torch.cuda.synchronize()
+    end_time = time.time()
+
+    if profiling:
+        print("profiling cpu time: ", end_time - start_time)
+        proton.finalize()
+    else:
+        print("pure cpu time: ", end_time - start_time)
+
+run(profiling=False)
+run(profiling=True)
