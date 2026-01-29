fc3603ace590d64dbda79df98bba17347d67c123
Jokeren <robinho364@gmail.com>
2026-01-29 15:50:01 -0500

    Add metadata function and Triton kernel for dot product

--- cudagraph.py
+++ cudagraph.py
@@ -2,6 +2,21 @@ import torch
 import triton 
 import triton.profiler as proton 
 import time
+from typing import NamedTuple
+
+
+def metadata_fn(grid: tuple, metadata: NamedTuple, args: dict):
+    vec = torch.arange(512, device="cuda")
+    return {"name": f"dot", "flops": 512 * 512 * 2, "vec": vec}
+
+
+@triton.jit(launch_metadata=metadata_fn)
+def triton_dot(a_ptr, b_ptr, c_ptr, BLOCK_SIZE: tl.constexpr):
+    offs = tl.arange(0, BLOCK_SIZE)
+    a = tl.load(a_ptr + offs[:, None] * BLOCK_SIZE + offs[None, :])
+    b = tl.load(b_ptr + offs[:, None] * BLOCK_SIZE + offs[None, :])
+    c = tl.dot(a, b)
+    tl.store(c_ptr + offs[:, None] * BLOCK_SIZE + offs[None, :], c)
 
 
 def fn(num_scopes=5000): 
@@ -11,11 +26,14 @@ def fn(num_scopes=5000):
             x = torch.randn(512, 512, device=device) 
             y = torch.randn(512, 512, device=device) 
             z = torch.relu(x @ y) 
+            triton_dot[(1, )](x, y, z, 512)
 
 
 def run(num_iters=100):
     # warmup
     fn()
+
+    session = proton.start("test", mode="periodic_flushing:format=hatchet_msgpack")
 
     stream = torch.cuda.Stream()
     torch.cuda.set_stream(stream)
@@ -27,18 +45,11 @@ def run(num_iters=100):
 
     for i in range(num_iters): 
         g.replay()
-
-        if i == num_iters - 1:
-            time0 = time.time()
-            proton.deactivate(0)
-            print(f"deactivate time: {time.time() - time0:.4f}")
-            time0 = time.time()
-            proton.get_data_msgpack(0)
-            print(f"get_data time: {time.time() - time0:.4f}")
-            proton.clear_data(0)
-            proton.activate(0)
+        if i != 0 and i % 10 == 0:
+            proton.data.advance_phase(session)
 
     torch.cuda.synchronize()
+    proton.finalize()
     end_time = time.time()
 
     print(f"cpu time: {end_time - start_time:.4f}")
